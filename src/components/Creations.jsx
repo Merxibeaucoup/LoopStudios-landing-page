8614e8144374d64dcfb0c8860db6eda6e6914151
Edgar Briandt <ebriandt_6243@email.ric.edu>
2023-03-04 23:47:21 -0500

    desktop started

--- src/components/Creations.jsx
+++ src/components/Creations.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { creationItems_mobile } from "../data/creationItems";
+import { creationItems_desktop } from "../data/creationItems";
 import CreationsList from "./CreationsList";
 
 const Creations = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth < 992);
+
+  const updateScreen = () => {
+    setDesktop(window.innerWidth < 992);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateScreen);
+    return () => window.removeEventListener("resize", updateScreen);
+  }, []);
+
   return (
     <>
       <section className="creations">
         <h2 className="creations__header">Our creations</h2>
 
         <div className="creations__list">
-          {creationItems_mobile.map((item) => {
-            return <CreationsList item={item} key={item.id} />;
-          })}
+          {/* if screen size < 992 show mobile images + data  , else show desktop images + data */}
+          {isDesktop
+            ? creationItems_mobile.map((item) => {
+                return <CreationsList item={item} key={item.id} />;
+              })
+            : creationItems_desktop.map((item) => {
+                return <CreationsList item={item} key={item.id} />;
+              })}
         </div>
         <div className="creations__button">
           <button className="btn">see all</button>
--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -5,18 +5,9 @@ const Showcase = () => {
     text: "Immersive experiences that deliver",
   });
 
-  /** Style */
-  const showcaseStyles = {
-    background: "url(/images/mobile/image-hero.jpg)",
-    backgroundRepeat: " no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "70rem",
-  };
-
   return (
     <>
-      <section className="showcase" style={showcaseStyles}>
+      <section className="showcase">
         <div className="showcase__overlay">
           <h1>{state.text}</h1>
         </div>
